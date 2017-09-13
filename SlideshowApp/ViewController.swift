0253447fb04d8009007dc31dae3ecff88cd81db9
Shoei Hirata <shoeihirata@ShoeinoMacBook-Pro.local>
2017-09-13 20:28:02 +0900

    ee

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
+    var timer: Timer!
     var images : [UIImage] = []
     var index = 0
-
-   
+    
+    @IBOutlet weak var playbutton: UIButton!
+    
     @IBOutlet weak var imageview: UIImageView!
     override func viewDidLoad(){
         super.viewDidLoad()
@@ -29,35 +30,71 @@ class ViewController: UIViewController {
         if index < 0 {
             index = images.count - 1
         }
-         imageview.image = images[index]
-}
-
+        imageview.image = images[index]
+    }
+    
     
     @IBAction func next(_ sender: Any) {
-
+        
         index += 1
         
         if images.count - 1 < index{
-        index = 0
+            index = 0
         }
-         imageview.image = images[index]
+        imageview.image = images[index]
+    
     }
+    
+    @IBOutlet weak var Back: UIButton!
 
-
-     override func didReceiveMemoryWarning() {
+    @IBOutlet weak var Next: UIButton!
+    
+    @IBAction func play(_ sender: Any){
+        if self.timer == nil {
+            self.timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(slide), userInfo: nil, repeats: true)
+            
+            Back.isEnabled = false
+            Next.isEnabled = false
+            
+            playbutton.setTitle("停止", for: .normal)
+            
+            
+        }
+        else {
+            self.timer.invalidate()
+            self.timer = nil
+            
+            playbutton.setTitle("再生", for: .normal)
+            
+            Back.isEnabled = true
+            Next.isEnabled = true
+        }
+        
+    }
+    func slide(){
+        index += 1
+        
+        if images.count - 1 < index{
+            index = 0
+        }
+        imageview.image = images[index]
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-     override func prepare(for segue: UIStoryboardSegue, sender: Any? ) {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any? ) {
         
         let imageViewController:ImageViewController = segue.destination as! ImageViewController
         
         imageViewController.image = images[index]
-
         
-}
+        
+    }
     
-    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+    @IBAction func unwind(segue: UIStoryboardSegue) {
     }
     
 }
